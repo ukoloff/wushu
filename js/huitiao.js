9b25255f0ae7f52ab1aa0ebee7f8328d5fda9082
Stas Ukolov <ukoloff@gmail.com>
2014-04-14 11:21:39 +0600

    Add form inputs

--- js/huitiao.js
+++ js/huitiao.js
@@ -11,10 +11,18 @@ setTimeout(function(){
     popup=document.createElement('div')
     popup.innerHTML='<h2>Обратный звонок<a href="#" class="close" title="Закрыть">&times;</a></h2>\
 <form>\
-Введите номер телефона\
+Укажите Ваш телефон<br>и мы Вам перезвоним:<br>\
+<label><div>Имя</div><input type="text" name="name"></label>\
+<label><div>Телефон</div><input type="text" name="tel"></label>\
+<label><div>E-mail</div><input type="text" name="mail"></label>\
+<label><div>Когда удобно звонить</div><input type="text" name="time"></label>\
+<label><div>Примечания</div><textarea name="notes"></textarea></label>\
+<br><input type="submit" value="Позвоните мне!">\
 </form>\
 '
     popup.getElementsByTagName('a')[0].onclick=hide
+    z=popup.getElementsByTagName('input')
+    z[z.length-1].onclick=hide
     return popup
   }
 
