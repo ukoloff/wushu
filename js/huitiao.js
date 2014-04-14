5fed6e56388918bceb53371160a487d2a507156a
Stas Ukolov <ukoloff@gmail.com>
2014-04-14 10:48:42 +0600

    Refactor popup create/delete code

--- js/huitiao.js
+++ js/huitiao.js
@@ -2,14 +2,20 @@ setTimeout(function(){
   var popup, a=document.getElementById('header').getElementsByTagName('a')[0]
 
   a.onclick=function(){
-    if(popup){
-      popup.remove()
-      popup=null
-      return false
-    }
+    if(popup) return hide()
+    a.parentElement.appendChild(linpen())
+    return false
+  }
+
+  function linpen(){
     popup=document.createElement('div')
     popup.innerHTML='Hi there!'
-    a.parentElement.appendChild(popup)
+    return popup
+  }
+
+  function hide(){
+    popup.remove()
+    popup=null
     return false
   }
 }, 0)
