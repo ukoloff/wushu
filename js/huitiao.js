c135e351315a470c3d81fbd5dcc860f854701c33
Stas Ukolov <ukoloff@gmail.com>
2014-04-14 15:18:52 +0600

    Get user input and check phone

--- js/huitiao.js
+++ js/huitiao.js
@@ -1,4 +1,6 @@
 setTimeout(function(){
+  var simpleFormToken='eb9a015c630904b0a5e1e3921dbc2b48'
+
   var popup, a=document.getElementById('header').getElementsByTagName('a')[0]
 
   a.onclick=function(){
@@ -13,15 +15,16 @@ setTimeout(function(){
 <form>\
 Укажите Ваш телефон<br>и мы Вам перезвоним:<br>\
 <label><div>Имя</div><input type="text" name="name"></label>\
-<label><div>Телефон</div><input type="text" name="tel" autofocus required></label>\
+<label><div>Телефон (<span class="text-error">обязательно</span>)</div><input type="text" name="tel" autofocus required></label>\
 <label><div>E-mail</div><input type="text" name="mail"></label>\
 <label><div>Когда удобно звонить</div><input type="text" name="time"></label>\
 <label><div>Примечания</div><textarea name="notes"></textarea></label>\
 <br><input type="submit" value="Позвоните мне!">\
+<div></div>\
 </form>\
 '
     popup.getElementsByTagName('a')[0].onclick=hide
-    popup.getElementsByTagName('form')[0].onsubmit=hide
+    popup.getElementsByTagName('form')[0].onsubmit=send
     return popup
   }
 
@@ -30,4 +33,28 @@ setTimeout(function(){
     popup=null
     return false
   }
+
+  function info(txt, klass)
+  {
+    var z=popup.getElementsByTagName('div')
+    z=z[z.length-1]
+    z.innerHTML=txt
+    if(klass) z.className=klass
+    return false
+  }
+
+  function send()
+  {
+    var x={form_api_token: simpleFormToken, url: location.href}
+    for(var i=this.length-1; i>=0; i--)
+      if(this[i].name) x[this[i].name]=this[i].value.replace(/^\s+|\s+$/g, '')
+    if(x.tel.replace(/\D+/g, '').length<7)
+    {
+      info('Неверный телефон', 'error')
+      this.tel.focus()
+      return false
+    }
+    return false
+  }
+
 }, 0)
